125d047914106d24b304c084eb9ec5338676e06e
ssk090 <shivanandasai.38@gmail.com>
2025-03-26 01:41:58 +0530

    feat: add rate limiting middleware to enhance API security

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express');
 const { userRouter } = require("./routes/userRouter");
 const { courseRouter } = require("./routes/courseRouter");
 const { adminRouter } = require("./routes/adminRouter");
+const rateLimit = require('express-rate-limit');
 const mongoose = require("mongoose");
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 10, // Limit each IP to 100 requests per windowMs
+    message: {
+        message: "Too many requests from this IP, please try again later."
+    }
+});
+
+app.use(limiter);
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
